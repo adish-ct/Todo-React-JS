e08a741c408f0eb6e8b357c987e7a87f4dd6673e
adish-ct <adishathu525@gmail.com>
2023-10-06 23:46:37 +0530

    input field focused , useRef and useEffect used

--- todo-app/src/components/Todo.jsx
+++ todo-app/src/components/Todo.jsx
@@ -2,15 +2,32 @@ import React, { useState, useEffect, useRef } from 'react'
 import './css/Todo.css'
 
 function Todo(props) {
+    // input state created to store data while
     const [input, setInput] = useState('')
-    const [todo, setTodo] = useState()
+    // todo state used to stored tasks in array format
+    const [todo, setTodo] = useState([])
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+    }
+
+    const addTodo = () => {
+        setTodo([...todo, input]);
+        setInput('');
+    }
+
+    const inputRef = useRef('null')
+
+    useEffect(() => {
+        inputRef.current.focus()
+    })
     return (
         <div className='container text-center p-5 mt-5'>
             <div className="text-center bg-secondary p-5 border-2">
                 <h1 className='mb-4 text-white'>TODO APP</h1>
-                <form action="" className='form-outline'>
-                    <input type="text" className='w-100 form-control m-2' value={input} placeholder='Enter your task' onChange={(event) => setInput(event.target.value)} />
-                    <button className='w-50 btn btn-dark mt-3 fw-bold'> SAVE TASK </button>
+                <form action="" className='form-outline' onSubmit={handleSubmit} >
+                    <input type="text" className='w-100 form-control m-2' value={input} ref={inputRef} placeholder='Enter your task' onChange={(event) => setInput(event.target.value)} />
+                    <button className='w-50 btn btn-dark mt-3 fw-bold' onClick={addTodo} > SAVE TASK </button>
                 </form>
             </div>
 
@@ -18,10 +35,8 @@ function Todo(props) {
                 <div className="text-center border-bottom border-bottom-danger">
                     <h3>MY TASKS</h3>
                 </div>
-                <ul>
-                    <li className='form-control mt-3'>First list</li>
-                    <li className='form-control '>Second list</li>
-                    <li className='form-control'>Third list</li>
+                <ul className='text-left'>
+                    {todo.map((task) => <li className='form-control task-control'><span className='icon-content'><i className="bi bi-star-fill"></i></span> {task} </li>)}
                 </ul>
             </div>
         </div>
