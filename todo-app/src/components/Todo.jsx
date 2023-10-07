2b271c9615d2c92229530d50c7d8638fdbbf05f6
adish-ct <adishathu525@gmail.com>
2023-10-07 18:50:31 +0530

    style updated

--- todo-app/src/components/Todo.jsx
+++ todo-app/src/components/Todo.jsx
@@ -66,38 +66,44 @@ function Todo(props) {
         inputRef.current.focus()
     })
     return (
-        <div className='container text-center p-5 mt-5'>
-            <div className="text-center bg-secondary p-5 border-2">
-                <h1 className='mb-4 text-white'>TODO APP</h1>
-                <form action="" className='form-outline' onSubmit={(e) => e.preventDefault()} >
-                    <input type="text" className='w-100 form-control m-2' value={input} ref={inputRef} placeholder='Enter your task' onChange={(event) => setInput(event.target.value)} />
-                    <button className='w-50 btn btn-dark mt-3 fw-bold' onClick={addTodo} > {taskId ? 'UPDATE TASK' : 'SAVE TASK'} </button>
-                </form>
+        <div className='body'>
+            <div className='text-center p-5 section-head'>
+                <h1 className='mb-4 text-white custom-head'>TODO</h1>
             </div>
+            <div className='container text-center p-5'>
+                <div className="text-center p-5 border-2 todo-section">
 
-            <div className='bg-warning mt-3 p-3'>
-                <div className="text-center border-bottom border-bottom-danger">
-                    <h3>MY TASKS</h3>
+                    <form action="" className='form-outline' onSubmit={(e) => e.preventDefault()} >
+                        <input type="text" className='w-100 form-control m-2 form-input' value={input} ref={inputRef} placeholder='Enter your task' onChange={(event) => setInput(event.target.value)} />
+                        <button className='w-50 btn btn-success mt-3 fw-bold submit-button' onClick={addTodo} > {taskId ? 'UPDATE TASK' : 'SAVE TASK'} </button>
+                    </form>
                 </div>
-                <ul className='text-left'>
-                    {todo.map((todoObj) => (
-                        <div className='todo-task'>
-                            <li className='form-control task-control' id={todoObj.status ? 'input-item' : ''}>
-                                <span className='icon-content'>
-                                    <i className="bi bi-star-fill"></i>
-                                </span> {todoObj.task}
-                            </li>
-                            <div className='todo-icons text-success'>
-                                <BsCheck2All className='icons' id='complete' onClick={() => taskComplete(todoObj.id)} title='Complete' />
-                                <BiEdit className='icons text-info' title='Edit' id='edit' onClick={() => taskEdit(todoObj.id)} />
-                                <AiFillDelete className='icons text-danger' title='Delete' id='delete' onClick={() => taskDelete(todoObj.id)} />
+
+                <div className='task-section mt-3 p-3'>
+                    <div className="text-center border-bottom border-bottom-danger">
+                        <h3 className='text-white'>MY TASKS</h3>
+                    </div>
+                    <ul className='text-left'>
+                        {todo.map((todoObj) => (
+                            <div className='todo-task'>
+                                <li className='form-control task-control' id={todoObj.status ? 'input-item' : ''}>
+                                    <span className='icon-content'>
+                                        <i className="bi bi-star-fill"></i>
+                                    </span> {todoObj.task}
+                                </li>
+                                <div className='todo-icons text-success'>
+                                    <BsCheck2All className='icons' id='complete' onClick={() => taskComplete(todoObj.id)} title='Complete' />
+                                    <BiEdit className='icons text-info' title='Edit' id='edit' onClick={() => taskEdit(todoObj.id)} />
+                                    <AiFillDelete className='icons text-danger' title='Delete' id='delete' onClick={() => taskDelete(todoObj.id)} />
+                                </div>
                             </div>
-                        </div>
-                    )
-                    )}
-                </ul>
+                        )
+                        )}
+                    </ul>
+                </div>
             </div>
         </div>
+
     )
 }
 
